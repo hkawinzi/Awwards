3c3836d791d21e1445a78870486323ea65111552
happiness <happinesskawinzi@gmail.com>
2019-10-26 12:17:16 +0300

    creating a profile class

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Create your models here
+class Profile(models.model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='picture/', blank=True)
+    bio = models.TextField(default='')
+    posted_project = models.ForeignKey('Project', on_delete=models.CASCADE)
+    contact_info = models.CharField(max_length=300, blank=True)
