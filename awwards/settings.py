1de8f59d46476ab6d85c4da2bf39a06bdbbae432
happiness <happinesskawinzi@gmail.com>
2019-10-27 00:14:23 +0300

    redirecting login and logout

--- awwards/settings.py
+++ awwards/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from decouple import config
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -40,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'crispy_forms',
     # 'tinymce',
 ]
 
@@ -133,3 +134,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = '/'
