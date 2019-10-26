fb0ab9a9f27bcccde88387b036567e4c0c947a6c
happiness <happinesskawinzi@gmail.com>
2019-10-26 21:11:34 +0300

    creating a view function that enables a user to sign up and cleans the form after form submission

--- app/views.py
+++ app/views.py
@@ -14,3 +14,14 @@ def index(request):
     else:
         form = PostForm()
     return render(request, 'main/index.html', {'posts': post, 'form': form})
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
