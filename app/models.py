71f8aa691b2d2e95dd4f1def6d56fb9f35468027
happiness <happinesskawinzi@gmail.com>
2019-10-26 12:37:39 +0300

    creating functions that will save,delete and update a profile

--- app/models.py
+++ app/models.py
@@ -4,8 +4,20 @@ from django.contrib.auth.models import User
 
 # Create your models here
 class Profile(models.model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    picture = models.ImageField(upload_to='picture/', blank=True)
+    user = models.OneToOneField(User)
+    image = models.ImageField(upload_to='picture/', blank=True)
     bio = models.TextField(default='')
     posted_project = models.ForeignKey('Project', on_delete=models.CASCADE)
     contact_info = models.CharField(max_length=300, blank=True)
+
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_bio(self,bio):
+        self.save()
