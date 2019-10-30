0254bada7870e63721afaa0db6828eb45cf68c6c
happiness <happinesskawinzi@gmail.com>
2019-10-30 10:32:00 +0300

    Fix app urls.

--- app/urls.py
+++ app/urls.py
@@ -1,17 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.conf.urls import url
 from . import views
 
 from .views import home, signup, Profile, edit_profile, upload, index
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('signup/', signup, name='signup'),
-    path('upload/', upload, name='upload'),
-    path('profile/<username>/', Profile, name='profile'),
-    path('profile/<username>/settings', edit_profile, name='edit'),
-    path('account/', include('django.contrib.auth.urls')),
+    url('', home, name='home'),
+    url('signup/', signup, name='signup'),
+    url('upload/', upload, name='upload'),
+    url('profile/<username>/', Profile, name='profile'),
+    url('profile/<username>/settings', edit_profile, name='edit'),
+    url('account/', include('django.contrib.auth.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
