87c255533d90c9dd373616c076290f4aec443430
happiness <happinesskawinzi@gmail.com>
2019-10-30 10:37:41 +0300

    Remove path imports on the app and project urls.

--- app/urls.py
+++ app/urls.py
@@ -1,18 +1,17 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import include
 from django.conf.urls import url
-from . import views
 
 from .views import home, signup, Profile, edit_profile, upload, index
 
 urlpatterns = [
-    url('', home, name='home'),
-    url('signup/', signup, name='signup'),
-    url('upload/', upload, name='upload'),
-    url('profile/<username>/', Profile, name='profile'),
-    url('profile/<username>/settings', edit_profile, name='edit'),
-    url('account/', include('django.contrib.auth.urls')),
+    url(r'', home, name='home'),
+    url(r'signup/', signup, name='signup'),
+    url(r'upload/', upload, name='upload'),
+    url(r'profile/<username>/', Profile, name='profile'),
+    url(r'profile/<username>/settings', edit_profile, name='edit'),
+    url(r'account/', include('django.contrib.auth.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- awwards/urls.py
+++ awwards/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include
 from django.conf.urls import url
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url('', include('app.urls'))
+    url(r'admin/', admin.site.urls),
+    url(r'', include('app.urls'))
 ]
